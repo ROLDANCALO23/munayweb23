791a6858f4d35721f1d046161a4ea558d51e8b56
Calo Andre Roldan Amau <roldan.calo23@gmail.com>
2024-02-01 17:15:43 -0500

    agregar seccion de nosotros

--- js/servicios.js
+++ js/servicios.js
@@ -1,11 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.from('.servicios-item', {
     opacity: 0,
-    duration: 2,
+    duration: 1.5,
+    stagger: 0.3,
     scrollTrigger: {
         trigger: '.servicios-item',
-        start: 'top 80%',
-        end: 'top 50%',
-        toggleActions: 'play none none none'
+        start: 'top 90%'
+    }
+});
+
+gsap.from('.us-img', {
+    opacity: 0,
+    duration: 2,
+    x:-120,
+    scrollTrigger: {
+        trigger: '.us-img',
+        start: 'top 90%'
+    }
+});
+
+gsap.from('.us-text', {
+    opacity: 0,
+    duration: 2,
+    x:120,
+    scrollTrigger: {
+        trigger: '.us-text',
+        start: 'top 90%'
     }
 });
