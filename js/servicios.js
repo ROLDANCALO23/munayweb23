6e6e816074ad043d24d129081283daebdb681edc
Calo Andre Roldan Amau <roldan.calo23@gmail.com>
2024-02-02 11:06:15 -0500

    cambios de diseño

--- js/servicios.js
+++ js/servicios.js
@@ -1,3 +1,4 @@
+let anchoPantalla = window.innerWidth;
 gsap.registerPlugin(ScrollTrigger);
 gsap.from('.servicios-item', {
     opacity: 0,
@@ -19,12 +20,27 @@ gsap.from('.us-img', {
     }
 });
 
-gsap.from('.us-text', {
-    opacity: 0,
-    duration: 2,
-    x:120,
-    scrollTrigger: {
-        trigger: '.us-text',
-        start: 'top 90%'
-    }
-});
+//validar si viewport es mayor a 768px
+if(anchoPantalla >= 768){
+    gsap.from('.us-text', {
+        opacity: 0,
+        duration: 2,
+        x:120,
+        scrollTrigger: {
+            trigger: '.us-text',
+            start: 'top 90%'
+        }
+    });
+
+}
+else{
+    gsap.from('.us-text', {
+        opacity: 0,
+        duration: 2,
+        y:120,
+        scrollTrigger: {
+            trigger: '.us-text',
+            start: 'top 90%'
+        }
+    });
+}
