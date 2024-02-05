764eedd0dd28c8878eef7f79e9ab01bdb4795c70
Calo Andre Roldan Amau <roldan.calo23@gmail.com>
2024-02-05 07:27:17 -0500

    correcciones animaciòn

--- js/animation.js
+++ js/animation.js
@@ -44,3 +44,18 @@ else{
         }
     });
 }
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+    //let timeLine = gsap.timeline();
+        gsap.from('.caracteristicas--card-item', {
+            stagger:0.5,
+            y: 100,
+            opacity: 0,
+            duration: 1.5,
+            delay:0.5,
+            scrollTrigger: {
+                trigger: '.caracteristicas--card-item'
+            }
+        })
